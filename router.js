de588eee657c805e50c0b5acfb486bb7b5b64ea6
Daniel Zhu <danielzhu777@gmail.com>
2017-10-07 23:00:21 -0700

    Added some post functionality

--- router.js
+++ router.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var fs = require("fs");
+var bodyParser = require('body-parser')
+router.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
 // GET '/ping'
 // Status 200 response if server is online
@@ -10,6 +14,7 @@ router.get('/ping', function(req, res){
 
 router.get('/trips', function(req, res){
   var total = [];
+  // make a hash table
   var trips = fs.readFileSync(__dirname + "/" + "trips.json");
   var jsonTrips = JSON.parse(trips);
   for (var i = 0; i < jsonTrips.trips.length; i++)
@@ -32,11 +37,12 @@ router.get('/trips', function(req, res){
       }
     }
   }
-  console.log(total);
+  console.log(JSON.stringify(total));
+  res.send(JSON.stringify(total));
   res.end();
 });
 
-router.get('/trips/:trip_id/leader/transaction', function(req, res){
+router.get('/trips/:trip_id/user_id=:user_id', function(req, res){
   var contents = fs.readFileSync(__dirname + "/" + "trips.json");
   var jsonContent = JSON.parse(contents);
   var index = 0;
@@ -44,10 +50,12 @@ router.get('/trips/:trip_id/leader/transaction', function(req, res){
     var temp = jsonContent.trips[i];
     if (req.params.trip_id == temp.id)
     {
+      res.end(JSON.stringify(temp));
       console.log(JSON.stringify(temp));
       index = i;
     }
   }
+  /*
   console.log();
   var transactions = fs.readFileSync(__dirname + "/" + "transactions.json");
   var jsonTransactions = JSON.parse(transactions);
@@ -57,13 +65,16 @@ router.get('/trips/:trip_id/leader/transaction', function(req, res){
     if (temp.id == req.params.trip_id)
     {
       console.log(JSON.stringify(temp.sub_transactions));
+      res.send(JSON.stringify(temp.sub_transactions));
       console.log();
     }
   }
+  */
   res.end();
 });
 
 router.get('/trips/:trip_id/leader/transactions', function(req, res){
+  var output = [];
   var transactions = fs.readFileSync(__dirname + "/" + "transactions.json");
   var jsonTransactions = JSON.parse(transactions);
   for (var i = 0; i < jsonTransactions.transaction.length; i++)
@@ -72,20 +83,26 @@ router.get('/trips/:trip_id/leader/transactions', function(req, res){
     if (temp.id == req.params.trip_id)
     {
       console.log(JSON.stringify(temp));
+      output.push(temp);
       console.log();
     }
   }
+  res.send(JSON.stringify(output));
   res.end();
 });
 
 router.get('/trips/:trip_id/leader/balance', function(req, res){
   var balances = fs.readFileSync(__dirname + "/" + "users.json");
   var jsonBalances = JSON.parse(balances);
+  var returnValue = [];
   for (var i = 0; i < jsonBalances.person.length; i++)
   {
-    console.log(jsonBalances.person[i].name);
-    console.log(jsonBalances.person[i].balance);
+    var output = [];
+    output.push(jsonBalances.person[i].name);
+    output.push(jsonBalances.person[i].balance);
+    returnValue.push(output);
   }
+  res.send(returnValue);
   res.end();
 });
 
@@ -109,11 +126,11 @@ router.get('/trips/:trip_id/transactions/user_id=:user_id', function(req, res){
     if (req.params.user_id == p.id)
     {
       var name = p.name;
-      console.log(name);
     }
   }
   var transactions = fs.readFileSync(__dirname + "/" + "transactions.json");
   var jsonTransactions = JSON.parse(transactions);
+  var output = [];
   for (var i = 0; i < jsonTransactions.transaction.length; i++)
   {
     var temp = jsonTransactions.transaction[i];
@@ -124,29 +141,71 @@ router.get('/trips/:trip_id/transactions/user_id=:user_id', function(req, res){
       {
         if(sub_trans[j].from == name)
         {
-          console.log(sub_trans[j]);
+          output.push(sub_trans[j]);
         }
       }
     }
   }
+  res.send(output);
   res.end();
 });
 
 router.post('/trips/:trip_id/deposit', function (req, res){
-
+  console.log(req.body);
+  var fileName = __dirname + "/" + "users.json";
+  var file = require(fileName);
+  file.person[req.body.user_id].balance += parseFloat(req.body.amount);
+  fs.writeFile(fileName, JSON.stringify(file), function (err) {
+    if (err) return console.log(err);
+    console.log(JSON.stringify(file, null, 2));
+    console.log('writing to ' + fileName);
+  });
+  res.send(JSON.stringify("true"));
+  res.end();
 });
 
 
 router.post('/trips/:trip_id/withdraw', function (req,res){
-
+  var fileName = __dirname + "/" + "users.json";
+  var file = require(fileName);
+  console.log(req.body);
+  file.person[req.body.user_id].balance -= parseFloat(req.body.amount);
+  if (file.person[req.body.user_id].balance < 0)
+  {
+    file.person[req.body.user_id].balance += req.body.amount;
+    res.sendStatus(400);
+    res.send(JSON.stringify("false"));
+    res.end();
+  }
+  fs.writeFile(fileName, JSON.stringify(file), function (err) {
+    if (err) return console.log(err);
+    console.log(JSON.stringify(file, null, 2));
+    console.log('writing to ' + fileName);
+  });
+  res.send(JSON.stringify("true"));
+  res.end();
 });
 
 router.post('/trips/:trip_id/charge', function (req,res){
+  var fileName = __dirname + "/" + "users.json";
+  var file = require(fileName);
+  console.log(req.params);
+  file.person[0].balance = 0; // replace this with the parameter contained in the request
 
+  fs.writeFile(fileName, JSON.stringify(file), function (err) {
+    if (err) return console.log(err);
+    console.log(JSON.stringify(file, null, 2));
+    console.log('writing to ' + fileName);
+  });
+  res.send(JSON.stringify("true"));
+  res.end();
+  // append to transactions.json as well
+  // append to trips.json as well
 });
 
 router.post('/trips/:trip_id/complete_trip', function (req,res){
-
+  // append to trips.json
+  // append to users.json
 });
 
 /*
